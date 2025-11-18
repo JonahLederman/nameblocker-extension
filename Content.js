6ef1f192d79ef0fe06f051e1c02a80961b3db223
Ev-ring <127316305+Ev-ring@users.noreply.github.com>
2025-11-18 12:19:30 -0500

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,8 +1,14 @@
 //rahhh
 
 const visibletext = document.body.innerText;
-const nameList = fetch('https://github.com/names.json')
-
+const nameList = fetch('https://github.com/first-names.txt')
+.then(response => response.json())
+      .then(data => {
+        console.log('User created:', data);
+      })
+      .catch(error => {
+        console.error('Error creating user:', error);
+      });
 
 for (txt of visibletext){
     if (txt == 
